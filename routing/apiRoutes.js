5528f79dba2ba2dc3dd785ce2702cb660e7d69ac
Hassanish <36898041+Hassanish@users.noreply.github.com>
2018-04-12 17:07:10 -0500

    Update apiRoutes.js

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,1 +1,30 @@
+var friendsData = require ("../data/friends.js");
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        var newFriendResponses = newFriend.scores;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            bestMatchDifference: 40
+        };
+        for (var i = 0; i < friendsData.length; i++) {
+            var diffCounter = 0;
+            for (var k = 0; k < friendsData[i].scores.length; k++) {
+               diffCounter += Math.abs(parseInt(friendsData[i].scores[k]) - parseInt(newFriendResponses[k]))              
+            }    
+            if (bestMatch.bestMatchDifference > diffCounter) {
+
+                bestMatch.name = friendsData[i].name;
+                bestMatch.photo = friendsData[i].photo;
+                bestMatch.bestMatchDifference = diffCounter;
+            }
+        }
+        (friendsData).push(newFriend);
+        res.json(bestMatch);
+    });
+};
 
